3b90bfcacb9281393a84cc85776d8acca1972c8a
Zachary Morel <morelz133@gmail.com>
2017-08-23 17:34:46 -0400

    working on proper getting itemId stored in purchases

--- routes/customer.js
+++ routes/customer.js
@@ -13,7 +13,22 @@ customer.get('/api/customer/items', (request, response) => {
     })
 })
 
-customer.post('/api/customer/items:ItemId/puschases', (request, response) => {
-  // Purchasing an Item
+customer.post('/api/customer/items/:id/purchases', (request, response) => {
+  // Purchasing an Item // <-- SELECT
+  models.Items.findById(request.params.id).then(item => {
+    let userMoney = request.body.amount - item.ItemCost
+    console.log(request.body.amount, item.ItemCost)
+    const purchase = models.Purchases.build({
+      MoneyInMachine: userMoney,
+      ItemId: item.id
+    })
+    purchase.save().then(moneyReturnToUser => {
+      response.json(moneyReturnToUser)
+    })
+  })
+
+  // <--- look at request body, and subract amound given from cost
+  // <--- take that number and INSERT a purchase
+  // response.json(purchase) <-- then respond with the purchase
 })
 module.exports = customer
